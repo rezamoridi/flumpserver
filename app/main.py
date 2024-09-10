2d38778c8918b3b23d195148eaa96a4c1fdc828f
rezamoridi <rezamoridi2042@gmail.com>
2024-09-10 10:59:39 -0700

    added base.metadata.create_all() needs to be replace with alembic

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
-import models
 import routers
-import models.users
 from fastapi import FastAPI
-from db import engine
-import routers.users
+
+import routers.auth
+import routers.song
+
+from db import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(routers.users.router, tags=["Users"])
-
-
-
+app.include_router(routers.auth.router, tags=["Users"])
+app.include_router(routers.song.router, tags=["Songs"])
