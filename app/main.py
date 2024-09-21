ac062b6dc11ee72e292c73138947a5a567ca7870
rezamoridi <rezamoridi2042@gmail.com>
2024-09-21 10:21:08 -0700

    mount static files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 import routers
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 
 import routers.auth
 import routers.song
+import routers.video
+import routers.book
+import frontRouters.front
 
 from db import Base, engine
 
@@ -10,5 +16,22 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify the origins you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(routers.auth.router, tags=["Users"])
 app.include_router(routers.song.router, tags=["Songs"])
+app.include_router(routers.video.router, tags=["Video"])
+app.include_router(routers.book.router, tags=["Books"])
+
+
+app.include_router(frontRouters.front.router, tags=["Front"])
